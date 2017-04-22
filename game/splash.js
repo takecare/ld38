7f954971af4101c91d9f033a55a682e4fd49a918
takecare <rui@vazteixeira.org>
2017-04-22 10:20:51 +0100

    add missing state

---
+++ game/splash.js
@@ -0,0 +1,17 @@
+import Phaser from 'phaser-ce';
+
+export default class extends Phaser.State {
+
+    init() {
+        // ...
+    }
+
+    preload() {
+        // ...
+    }
+
+    create() {
+        // ...
+    }
+
+}
