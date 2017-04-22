10433cc6aa712ee7d4de172bc0ad909acaaf08bf
takecare <rui@vazteixeira.org>
2017-04-22 10:21:09 +0100

    tune up eslint cfg and fix errors

--- game/game.js
+++ game/game.js
@@ -1,17 +1,23 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser-ce';
+
+import Loading from './Loading';
+import Splash from './Splash';
+import Menu from './Menu';
+import Play from './Play';
+import GameOver from './GameOver';
 
 export default class Game extends Phaser.Game {
 
-  constructor(width, height) {
-    super(width, height, Phaser.CANVAS, 'content', null)
+    constructor(width, height) {
+        super(width, height, Phaser.CANVAS, 'content', null);
 
-    this.state.add('Loading', Loading, false);
-    this.state.add('Splash', Splash, false);
-    this.state.add('Menu', Menu, false);
-    this.state.add('Play', Play, false);
-    this.state.add('GameOver', GameOver, false);
+        this.state.add('Loading', Loading, false);
+        this.state.add('Splash', Splash, false);
+        this.state.add('Menu', Menu, false);
+        this.state.add('Play', Play, false);
+        this.state.add('GameOver', GameOver, false);
 
-    this.state.start('Loading')
-  }
+        this.state.start('Loading');
+    }
 
 }
--- game/main.js
+++ game/main.js
@@ -1,6 +1,7 @@
-import Game from './Game'
+import Game from './Game';
 
 window.onload = function () {
     // TODO consider wrapper for Game instead of extension
+    // eslint-disable-next-line no-unused-vars
     const game = new Game(800, 600);
 };
