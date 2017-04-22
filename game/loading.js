80b8468072ff2cfd0aa699ea4894075e71e2fb8f
takecare <rui@vazteixeira.org>
2017-04-22 23:37:01 +0100

    add splash and loading

--- game/loading.js
+++ game/loading.js
@@ -4,21 +4,42 @@ export default class extends Phaser.State {
 
     init() {
         this.stage.backgroundColor = '#ccc';
-        // TODO set preloading sprite
+
+        this.game.load.onFileComplete.add(this.loadedFile, this);
+        this.game.load.onLoadComplete.add(this.finishedLoading, this);
+
+        //this.game.load.setPreloadSprite(sprite, direction)
     }
 
     preload() {
-        // TODO load loading bar sprite
+        this.game.load.image('loading', '../assets/loading.png');
+
         this.game.load.tilemap('map', '../assets/tilemap.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('tileset', '../assets/tileset.png');
 
         this.game.load.spritesheet('box', '../assets/box.png', 64, 64);
+
+        this.game.load.image('splash', '../assets/splash.png');
+
+        // this.game.add.text(this.game.world.centerX - 300, 0, 'text', '');
     }
 
     create() {
         // ...
 
-        this.state.start('Menu');
+        //const image = this.game.add.image('loading', '../assets/loading.png');
+        this.game.add.image(this.game.world.centerX / 2, this.game.world.centerX / 2, 'loading');
+
+        //this.game.load.isLoading
+        //this.game.load.hasLoaded
+        //this.game.load.progress
     }
 
+    loadedFile(progress, cacheKey, success, totalLoaded, totalFiles) {
+        //console.log(progress, cacheKey, success, totalLoaded, totalFiles);
+    }
+
+    finishedLoading() {
+        this.state.start('Splash');
+    }
 }
--- game/splash.js
+++ game/splash.js
@@ -1,19 +1,29 @@
 import Phaser from 'phaser-ce';
 
+const SPLASH_TIMEOUT_MILLIS = 0;//3000;
+
 export default class extends Phaser.State {
 
-    // TODO
-
     init() {
-        this.stage.backgroundColor = '#0f0';
+        this.stage.backgroundColor = '#ffffffff';
     }
 
     preload() {
-        // ...
+        //this.splashImage = this.game.cache.getImage('splash');
     }
 
     create() {
-        this.state.start('Loading');
+        //this.splashImage.anchor.set(0.5);
+        const image = this.game.add.image(this.game.world.centerX / 2, this.game.world.centerX / 2, 'splash');
+        image.anchor.set(0.5);
+
+        // TODO fix position
+
+        this.game.time.events.add(SPLASH_TIMEOUT_MILLIS, this.moveToMenu, this);
+    }
+
+    moveToMenu() {
+        this.state.start('Menu');
     }
 
 }
